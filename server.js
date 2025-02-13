d56dcbb9a103738e3f52c8f73ea10af672117697
Carlos <61396980+gerardoc96@users.noreply.github.com>
2025-02-13 15:53:07 -0800

    Created the Express code to route all endpoints listed.
    - Updated documentation where needed
    -Updated index.js to server.js
    -installed uuid & nodemon

---
+++ server.js
@@ -0,0 +1,191 @@
+const express = require("express"),
+  morgan = require('morgan'),
+  fs = require('fs'),
+  path = require('path'),
+  bodyParser = require('body-parser'),
+  uuid = require('uuid');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), { flags: 'a' });
+app.use(morgan('combined', { stream: accessLogStream }));
+
+let users = [
+  {
+    id: 1,
+    username: "Kim",
+    favoriteMovie: []
+  },
+  {
+    id: 2,
+    username: "Joe",
+    favoriteMovie: ["The fountain"]
+  }
+]
+
+let movies = [
+  {
+    "Title": "The Fountain",
+    "Description": "Movie Description",
+    "Genre": {
+      "Type": "Drama",
+      "Description": "Description of genre"
+    },
+    "Director": {
+      "Name": "Darren Aronofsky",
+      "Bio": "Director's Bio",
+      "Birth": 1969
+    },
+    "ImageURL": "image url",
+    "Featured": false
+
+  },
+  {
+    "Title": "The Princess Bride",
+    "Description": "Movie Description",
+    "Genre": {
+      "Type": "Action",
+      "Description": "Description of genre"
+    },
+    "Director": {
+      "Name": "Rob Reiner",
+      "Bio": "Director's Bio",
+      "Birth": 1947
+    },
+    "ImageURL": "image url",
+    "Featured": false
+
+  },
+
+]
+
+// GET all movies
+app.get('/movies', (req, res) => {
+  res.status(200).json(movies);
+});
+
+// GET movie by title
+app.get('/movies/:title', (req, res) => {
+  const { title } = req.params;
+  const movie = movies.find(movie => movie.Title === title);
+
+  if (movie) {
+    res.status(200).json(movie);
+  } else {
+    res.status(400).send('no such movie')
+  }
+});
+
+// GET movies by genre
+app.get('/movies/genre/:genreName', (req, res) => {
+  const { genreName } = req.params;
+  const genre = movies.find(movie => movie.Genre.Type === genreName).Genre;
+
+  if (genre) {
+    res.status(200).json(genre);
+  } else {
+    res.status(400).send('no such genre')
+  }
+})
+
+// GET directors information
+app.get('/movies/directors/:directorName', (req, res) => {
+  const { directorName } = req.params;
+  const director = movies.find(movie => movie.Director.Name === directorName).Director;
+
+  if (director) {
+    res.status(200).json(director);
+  } else {
+    res.status(400).send('no such director')
+  }
+});
+
+
+// Create User
+app.post('/users', (req, res) => {
+  const newUser = req.body;
+
+  if (newUser.name) {
+    newUser.id = uuid.v4();
+    users.push(newUser);
+    res.status(201).json(newUser);
+  } else {
+    res.status(400).send("users need names")
+  }
+
+})
+
+// Update user's username
+app.put('/users/:id', (req, res) => {
+  const { id } = req.params;
+  const updatedUser = req.body;
+
+  let user = users.find(user => user.id == id);
+
+  if (user) {
+    user.name = updatedUser.name;
+    res.status(200).json(user);
+  } else {
+    res.status(400).send('no such user')
+  }
+})
+
+// Adds movie to user's favorite movies
+app.post('/users/:id/:movieTitle', (req, res) => {
+  const { id, movieTitle } = req.params;
+
+  let user = users.find(user => user.id == id);
+
+  if (user) {
+    user.favoriteMovie.push(movieTitle)
+    res.status(200).send(`${movieTitle} has been added to user ${id}'s favorite movies`);
+  } else {
+    res.status(400).send('no such user')
+  }
+})
+
+// Delets movie from user's favorite movies
+app.delete('/users/:id/:movieTitle', (req, res) => {
+  const { id, movieTitle } = req.params;
+
+  let user = users.find(user => user.id == id);
+
+  if (user) {
+    user.favoriteMovie = user.favoriteMovie.filter(title => title !== movieTitle);
+    res.status(200).send(`${movieTitle} has been removed from user ${id}'s favorite list`);
+  } else {
+    res.status(400).send('no such user')
+  }
+})
+
+// Delets user's email
+app.delete('/users/:id', (req, res) => {
+  const { id } = req.params;
+
+  let user = users.find(user => user.id == id);
+
+  if (user) {
+    users = users.filter(user => user.id != id);
+    res.status(200).send(`user Email has been removed`);
+  } else {
+    res.status(400).send('no such user')
+  }
+})
+
+app.get('/', (req, res) => {
+  res.send('Welcome to MyFlix');
+})
+
+app.use(express.static('public'));
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+// Listen for request
+app.listen(8080, () => {
+  console.log('Your app is listening on port 8080.');
+});
