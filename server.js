51c0b7424e0b2c551e7e50104ef768f6d76dae4d
Carlos <61396980+gerardoc96@users.noreply.github.com>
2025-03-06 20:25:34 -0800

    Updated listening port

--- server.js
+++ server.js
@@ -233,6 +233,7 @@ app.delete('/users/:Username', passport.authenticate('jwt', { session: false }),
 }));
 
 // Listen for request
-app.listen(8080, () => {
-  console.log('Your app is listening on port 8080.');
+const port = process.env.PORT || 8080;
+app.listen(port, '0.0.0.0', () => {
+  console.log('Listening on Port ' + port);
 });
